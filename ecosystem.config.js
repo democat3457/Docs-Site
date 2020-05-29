dd7a51a8188b9f51bf74f4e9ef668fe6121c64fd
Jared <jaredlll08@gmail.com>
2020-05-29 23:46:46 +0200

    create pm2 ecosystem file and update script

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "apps": [
+        {
+            "name": "crafttweaker-docs-site",
+            "cwd": ".",
+            "script": "npm",
+            "args": "start",
+            "time": true
+        }
+    ]
+
+};
