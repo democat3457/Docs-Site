2e0cdc3ab241d2c53c43f4c52ca922735847bb3b
Jared <jaredlll08@gmail.com>
2020-06-16 20:11:29 +0200

    Store cookies forever. Fixes theme settings getting cleared

--- pages/api/set_theme.ts
+++ pages/api/set_theme.ts
@@ -1,4 +1,4 @@
-import { serialize } from 'cookie';
+import { CookieSerializeOptions, serialize } from 'cookie';
 import { NextApiRequest, NextApiResponse } from "next";
 
 module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -6,6 +6,8 @@ module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
     res.writeHead(400).end(`Bad request! hljsStyle: ${req.body["hljsStyle"]} pageTheme: ${req.body["pageTheme"]}`);
     return;
   }
-  res.setHeader('Set-Cookie', [serialize('hljsStyle', req.body["hljsStyle"], { path: '/' }), serialize('pageTheme', req.body["pageTheme"], { path: '/' })]);
+  let expiresAt: Date = new Date(9999, 12, 31, 23, 59, 59);
+  let options: CookieSerializeOptions = { path: '/', expires: expiresAt };
+  res.setHeader('Set-Cookie', [serialize('hljsStyle', req.body["hljsStyle"], options), serialize('pageTheme', req.body["pageTheme"], options)]);
   res.end();
 };
