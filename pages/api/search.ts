9afcc1b7c5289e65352a2c6ee99a5788b0e0b8cb
Jared <jaredlll08@gmail.com>
2020-06-17 11:25:51 +0200

    edit distance, not boost

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -70,8 +70,8 @@ module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
     });
   }
   // Makes the search a bit more greedy, may need to adjust number in the future
-  if (query.indexOf("^") !== -1) {
-    query += "^8"
+  if (query.indexOf("~") !== -1) {
+    query += "~8"
   }
   let results = index.search(query);
   let returned = [];
