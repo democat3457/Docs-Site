7c4d9d87f8745bf5bd21bcff65f46a832239f24e
Jared <jaredlll08@gmail.com>
2020-06-17 16:41:27 +0200

    Fixed external links breaking

--- components/markdown/Clink.tsx
+++ components/markdown/Clink.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import { CLinkProps } from "../../utils/Interfaces";
 
 export default function Clink({ href, children }: CLinkProps) {
+  if (href.startsWith("http")) {
+    return <a className = {`text-blue-700 dark:text-blue-300 break-all`} href = {href}>
+      {children}
+    </a>
+  }
   return (
     <Link href = {`/[version]/[lang]/[...slug]`} as = {href}>
 
