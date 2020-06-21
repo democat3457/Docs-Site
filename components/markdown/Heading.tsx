94309780e758bb1864711bbaaa54847ea51c2f27
Jared <jaredlll08@gmail.com>
2020-06-21 21:52:18 +0200

    "fixed" headings sometimes crashing

--- components/markdown/Heading.tsx
+++ components/markdown/Heading.tsx
@@ -5,27 +5,27 @@ function makeHeader(level: number, id: string, child: ReactNode[]) {
 
   switch (level) {
     case 1:
-      return <h1 id = {id}>
+      return <h1 id={id}>
         {child}
       </h1>
     case 2:
-      return <h2 id = {id}>
+      return <h2 id={id}>
         {child}
       </h2>
     case 3:
-      return <h3 id = {id}>
+      return <h3 id={id}>
         {child}
       </h3>
     case 4:
-      return <h4 id = {id}>
+      return <h4 id={id}>
         {child}
       </h4>
     case 5:
-      return <h5 id = {id}>
+      return <h5 id={id}>
         {child}
       </h5>
     case 6:
-      return <h6 id = {id}>
+      return <h6 id={id}>
         {child}
       </h6>
   }
@@ -33,9 +33,12 @@ function makeHeader(level: number, id: string, child: ReactNode[]) {
 
 export default function Heading({ level, children }: HeadingProps) {
   let child: any = children[0];
-  let id = child.props.value.toLowerCase().replace(/[^a-zA-Z0-9_-]/gmi, "-");
+  let id = "invalid";
+  if (child.props.value) {
+    id = child.props.value.toLowerCase().replace(/[^a-zA-Z0-9_-]/gmi, "-");
+  }
 
-  return <a href = {`#${id}`} className={`block`}>
+  return <a href={`#${id}`} className={`block`}>
     {makeHeader(level, id, child)}
   </a>
 
